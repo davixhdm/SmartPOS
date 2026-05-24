d699a418d65654fb7c411027093654335be7ec47
Davix HDM <davismcintyre5@gmail.com>
2026-05-24 19:34:31 +0300

    Updated Code

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// client/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,11 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: import.meta.env.DEV ? {
+    proxy: {
       "/api": {
         target: "http://localhost:5000",
         changeOrigin: true,
       },
-    } : undefined,
+    },
   },
 });
